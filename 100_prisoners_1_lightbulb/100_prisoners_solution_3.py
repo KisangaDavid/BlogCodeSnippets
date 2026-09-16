0b485fad85d5b1e72258b0f6c51fc8c323994c59
David Kisanga <davidlivingstonekisanga@gmail.com>
2026-09-15 23:51:48 -0400

    added solution 3 code snippet

--- 100_prisoners_1_lightbulb/100_prisoners_solution_3.py
+++ 100_prisoners_1_lightbulb/100_prisoners_solution_3.py
@@ -2,6 +2,7 @@ import random
 
 NUM_ITERATIONS = 1000
 NUM_PRISONERS = 100
+PHASE_ONE_LENGTH = 100
 
 simulated_results = []
 
@@ -21,11 +22,20 @@ def num_days_for_counter(counting_prisoner):
         if chosen_prisoner == counting_prisoner:
             return num_days
         num_days += 1
-        
+
+def execute_phase_one():
+    counted_prisoners = set()
+    for _ in range(0, PHASE_ONE_LENGTH):
+        chosen_prisoner = random.randint(1, NUM_PRISONERS)
+        if chosen_prisoner in counted_prisoners:
+            return counted_prisoners, chosen_prisoner
+        counted_prisoners.add(chosen_prisoner)
+    return counted_prisoners, chosen_prisoner 
+    
 for i in range(0, NUM_ITERATIONS):
-    counting_prisoner = random.randint(1, NUM_PRISONERS)
-    counted_prisoners = {counting_prisoner}
-    num_days_for_escape, counting_prisoner_count = 1, 1
+    counted_prisoners, counting_prisoner = execute_phase_one()
+    num_days_for_escape = PHASE_ONE_LENGTH
+    counting_prisoner_count = len(counted_prisoners)
     while counting_prisoner_count < NUM_PRISONERS:
         num_days_for_escape += num_days_for_new_prisoner(counted_prisoners)
         num_days_for_escape += num_days_for_counter(counting_prisoner)
